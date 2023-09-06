3fbb32b0cca9d1a6e38afc0cb3895dd2651570eb
Sonny Sangha - PAPA React <76973017+PapaReact@users.noreply.github.com>
2023-09-06 12:26:16 +0400

    Update functions.test.js

--- functions.test.js
+++ functions.test.js
@@ -7,3 +7,5 @@ test("adds 1 + 2 to equal 3", () => {
 test("adds 1 + 3 to NOT equal 3", () => {
   expect(sum(1, 3)).not.toBe(3);
 });
+
+// Simple run 'npm run test' in the terminal below to test your solution!
