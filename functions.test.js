46700ab9108cbfcaa0accc74a1f31c51ff0868c5
Sonny Sangha <sonny.sangha@gmail.com>
2023-09-06 12:00:20 +0400

    init

---
+++ functions.test.js
@@ -0,0 +1,9 @@
+const sum = require("./functions");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test("adds 1 + 3 to NOT equal 3", () => {
+  expect(sum(1, 3)).not.toBe(3);
+});
